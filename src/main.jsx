16a7604a735c869b7427d1e373dbc3ec78904390
prakharujn1 <128589011+prakharujn1@users.noreply.github.com>
2025-07-23 15:14:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { WebinarContextProvider } from "./context/WebinarsContext";
 import { ProjectsContextProvider } from './context/ProjectContext.jsx';
 import { Toaster } from 'react-hot-toast';
 
-export const server = "https://agility-server.onrender.com";
+export const server = "https://agilityai-server.onrender.com";
 
 
 createRoot(document.getElementById('root')).render(
